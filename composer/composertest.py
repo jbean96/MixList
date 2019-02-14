e2f521e4e717fb4e1597eeb58e48058795bdf89e
Jeremy Cruz <jwcruz@uw.edu>
2019-02-14 12:47:56 -0800

    added align tracks and tempo match to composer. script command SelectTime not supporting float

--- composer/composertest.py
+++ composer/composertest.py
@@ -1,6 +1,35 @@
 from composer import composer
 
-c = composer()
+song0 = {
+    'start_intro': 0.030,
+    'end_intro': 0.030,
+    'start_outro': 4.32,
+    'end_outro' : 8.69,
+    'tempo' : 110,
+    'offset' : 0
+    }
+song1 = {
+    'start_intro': 0.030,
+    'end_intro': 4.030,
+    'start_outro': 8.03,
+    'end_outro': 12.03,
+    'tempo': 120
+    }
+song2 = {
+    'start_intro': 0.030,
+    'end_intro': 3.87,
+    'start_outro': 3.87,
+    'end_outro': 15.3,
+    'tempo': 125
+}
+songs = [song0, song1, song2]
+
+c = composer(songs)
+c.new()
 c.importaudio()
-c.crossfade("0","1","10","18")
-c.exportaudio()
+c.alignsongs()
+
+for i in range(len(songs) - 1):
+    c.crossfade(i, i+1, songs[i], songs[i+1])
+    c.tempomatch(i, i+1, songs[i], songs[i+1])
+
