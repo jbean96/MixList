53227fbb19f334cd96830cdf8f36cf39521a2817
Joshua Bean <jbean96@cs.washington.edu>
2019-02-19 15:17:13 -0800

    Minor change

--- analyzer/analyzer/usersong.py
+++ analyzer/analyzer/usersong.py
@@ -57,7 +57,7 @@ class UserSong(Song):
         if sp_song is not None:
             matcher.merge_song_analysis(self, sp_song[0])
 
-        time_signature = self.get_analysis_feature(analysis.Feature.TIME_SIGNATURE)
+        time_signature = sp_song.get_analysis_feature(analysis.Feature.TIME_SIGNATURE)
         # Only if we get a non 4 beats per measure time signature do we re-annotate the beats
         if time_signature is not None and time_signature > 0 and time_signature != 4:
             beats = self.get_analysis_feature(analysis.Feature.BEATS)
