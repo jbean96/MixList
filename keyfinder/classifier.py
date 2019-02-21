973b5e62868c9afa49e4673676bfad5c9888787c
Joshua Bean <jbean96@cs.washington.edu>
2019-02-20 16:21:39 -0800

    Change to classifier going to test on VM

--- keyfinder/classifier.py
+++ keyfinder/classifier.py
@@ -16,35 +16,49 @@ from analyzer.analyzer import keys
 class SquashMethod(Enum):
     NORMALIZE = auto()
     AVERAGE = auto()
+    SUM = auto()
 
 def squash_chroma_matrix(chroma_cq: np.ndarray, method: SquashMethod) -> np.ndarray:
     if method == SquashMethod.NORMALIZE:
-        sum_rows = list(map(sum, chroma_cq))
-        max_val = max(sum_rows)
-        normalized_rows = list(map(lambda x: x / max_val, sum_rows))
-        return normalized_rows
+        sum_rows = np.sum(chroma_cq, axis=1)
+        max_val = np.max(sum_rows)
+        return np.apply_along_axis(lambda x: x / max_val, axis=0, arr=sum_rows)
     elif method == SquashMethod.AVERAGE:
-        return list(map(lambda x: sum(x) * 1.0 / len(x), chroma_cq))
+        return np.apply_along_axis(lambda x: np.sum(x) / len(x), axis=1, arr=chroma_cq)
+    elif method == SquashMethod.SUM:
+        return np.sum(chroma_cq, axis=1)
     else:
         raise ValueError("No squash method defined for %s" % method)
 
-def get_score(chroma_vector: np.ndarray, tone_profile: np.ndarray) -> float:
-    if len(chroma_vector) != util.SEMITONES:
-        raise ValueError("The length of the chroma vector is %d, should be %d" % (len(chroma_vector, util.SEMITONES)))
+def get_score(chroma_vector: np.ndarray, tone_profile: np.ndarray, octaves: int) -> float:
+    if len(chroma_vector) != util.SEMITONES * octaves:
+        raise ValueError("The length of the chroma vector is %d, should be %d" % (len(chroma_vector), util.SEMITONES * octaves))
     if len(tone_profile) != util.SEMITONES:
-        raise ValueError("The length of the tone profile vector is %d, should be %d" % (len(tone_profile, util.SEMITONES)))
+        raise ValueError("The length of the tone profile vector is %d, should be %d" % (len(tone_profile), util.SEMITONES))
     
-    chroma_vector_length = np.sum(np.square(chroma_vector))
-    tone_profile_length = np.sum(np.square(tone_profile))
+    tone_profile_extended = np.array([])
+    for i in range(octaves):
+        tone_profile_extended = np.concatenate((tone_profile_extended, tone_profile * tone_profiles.OCTAVE_WEIGHTS[i]))
+    assert len(chroma_vector) == len(tone_profile_extended) # sanity check
+    chroma_vector_length = np.sqrt(chroma_vector.dot(chroma_vector))
+    tone_profile_length = np.sqrt(tone_profile_extended.dot(tone_profile_extended))
     
-    dot_product = np.dot(chroma_vector, tone_profile)
+    dot_product = np.dot(chroma_vector, tone_profile_extended)
 
-    return dot_product / (chroma_vector_length + tone_profile_length)
+    return dot_product / (chroma_vector_length * tone_profile_length)
 
 DEFAULT_SQUASH_METHOD = SquashMethod.NORMALIZE
 
-def classify(samples: np.ndarray, sample_rate: int) -> keys.Camelot:
-    chroma_cq = librosa.feature.chroma_cqt(y=samples, sr=sample_rate)
+def classify(samples: np.ndarray, sample_rate: int, octaves: int) -> keys.Camelot:
+    if octaves <= 0 or octaves > util.OCTAVES:
+        raise ValueError("Octaves must be > 0 and <= %d, you provided: %d" % (util.OCTAVES, octaves))
+    if sample_rate <= 0:
+        raise ValueError("Sample rate must be > 0, you provided: %d" % sample_rate)
+
+    if octaves == 1:
+        chroma_cq = librosa.feature.chroma_cqt(y=samples, sr=sample_rate)
+    else: 
+        chroma_cq = librosa.core.cqt(y=samples, sr=sample_rate, n_bins=util.SEMITONES * octaves, bins_per_octave=util.SEMITONES)
     squashed = squash_chroma_matrix(chroma_cq, DEFAULT_SQUASH_METHOD)
     scores = {}
     for mode in keys.Mode:
@@ -52,14 +66,14 @@ def classify(samples: np.ndarray, sample_rate: int) -> keys.Camelot:
             if key == keys.Key.NO_KEY:
                 continue
             
-            scores[(key, mode)] = get_score(squashed, tone_profiles.get_tone_profile(key, mode))
+            scores[(key, mode)] = get_score(squashed, tone_profiles.get_tone_profile(key, mode), octaves)
     
     max_key = max(scores.keys(), key=lambda x: scores[x])
     return keys.CAMELOT_KEYS[max_key]
 
 def main(args: argparse.Namespace):
     samples, sr = librosa.load(args.file_path)
-    print(classify(samples, sr))
+    print(classify(samples, sr, 6))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Get the key classification of a song")
