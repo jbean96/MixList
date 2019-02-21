40d43b81928cde5483b5679cb4fb4f97a9365ac5
Jeremy Cruz <jwcruz@uw.edu>
2019-02-21 13:16:10 -0800

    changed optimizer output structure

--- composer/import_export_test.py
+++ composer/import_export_test.py
@@ -6,11 +6,11 @@ import composer
 from optimizer import optimizer
 from analyzer.analyzer import usersong
 
-songs_path = os.path.join("..", "testmp3s/songs")
+songs_path = os.path.join("..", "testmp3s")
 
-song_a = usersong.UserSong(os.path.join(songs_path, "edm", "1-01 Heartbreak Back (R3HAB Remix).mp3"), True)
-song_b = usersong.UserSong(os.path.join(songs_path, "edm", "1-01 Limbo.mp3"), True)
-song_c = usersong.UserSong(os.path.join(songs_path, "edm", "1-01 Meet In The Middle Ekali Remix.mp3"), True)
+song_a = usersong.UserSong(os.path.join(songs_path, "110bpm_8bars.mp3"), True)
+song_b = usersong.UserSong(os.path.join(songs_path, "120bpm_8bars.mp3"), True)
+song_c = usersong.UserSong(os.path.join(songs_path, "125bpm_8bars.mp3"), True)
 
 mix = optimizer.Optimizer.generate_3_track(song_a, song_b, song_c)
 
