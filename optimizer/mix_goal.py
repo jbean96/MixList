cec87e7f35da267f06be868b8af53c2fc2541be7
Gerard <gerar231@gmail.com>
2019-03-14 05:28:46 -0700

    Added proper feature scale.

--- optimizer/mix_goal.py
+++ optimizer/mix_goal.py
@@ -7,6 +7,8 @@ class MixGoal(object):
     pace (fast <--> slow)
     time (when should this happen?)
     """
+    FEATURE_SCALE = 10.0
+
     def __init__(self, energy: float, danceability: float, valence: float, pace: float, time_stamp: float):
         """
         Intializes a mix_goal instance
@@ -15,8 +17,8 @@ class MixGoal(object):
         assert danceability >= 0.0 and danceability <= 10.0
         assert valence >= 0.0 and valence <= 10.0
         assert pace >= 0.0 and pace <= 10.0
-        self.energy = energy
-        self.dance = danceability
-        self.valence = valence
+        self.energy = energy / self.FEATURE_SCALE
+        self.dance = danceability / self.FEATURE_SCALE
+        self.valence = valence / self.FEATURE_SCALE
         self.pace = pace
         self.time = time_stamp
